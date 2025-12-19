ed6d34aaaf0e017b94774d3cd2063448c740655d
Sahil Mali <sahilmali3107@gmail.com>
2025-12-19 15:26:00 +0530

    create User registeration process

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,75 @@
+import User from "../models/user.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
+
+const generateAccessAndRefreshToken = async userId => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access Token");
+    }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body;
+
+    const userExists = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if (userExists) {
+        throw new ApiError(409, "User with email or username already exists", []);
+    }
+
+    const user = await User.create({
+        email,
+        password,
+        username,
+        isEmailVerified: false,
+    });
+
+    const { unhashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
+
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+
+    user.save({ validateBeforeSave: false });
+
+    await sendEmail({
+        email: user?.email,
+        subject: "Please verify your Email",
+        mailgenContent: emailVerificationMailgenContent(
+            user?.username,
+            `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unhashedToken}`,
+        ),
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -emailVerificationToken -emailVerificationExpiry -refreshToken",
+    );
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went worng while registering the user.");
+    }
+
+    return res
+        .statusCode(201)
+        .json(
+            new ApiResponse(
+                200,
+                { user: createdUser },
+                "User registered successfully and verification email has been sent on your email",
+            ),
+        );
+});
+
+export { registerUser };
