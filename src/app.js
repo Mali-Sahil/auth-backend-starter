7275f3061f83603dea50de8263112c78534aadc4
Sahil Mali <sahilmali3107@gmail.com>
2025-12-20 15:33:02 +0530

    import cookie-parser in app.js and use it

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from 'cookie-parser'
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.use(cookieParser);
 
 // basic config
 app.use(express.json({ limit: "16kb" }));
